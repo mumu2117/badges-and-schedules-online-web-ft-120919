49c0a53e8c4d9cc95a871d47b1e0abe833271296
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 12:46:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,12 @@ def batch_badge_creator(attendees)
    attendees.map { |badges| badge_maker(badges) }
 end
   
+def assign_rooms(name)
+  rooms = []
+
+  name.each.with_index(1) do |name, room_assignment|
+    rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
+  end
+  rooms
+end
+
